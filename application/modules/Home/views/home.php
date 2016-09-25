caf5619cd961c059dc431620aaab871d39172ee7
00coll <s.eliseu.santos@gmail.com>
2016-09-25 16:22:26 -0300

    Correção de placeholder

--- application/modules/Home/views/home.php
+++ application/modules/Home/views/home.php
@@ -5,10 +5,7 @@
     <li class=""><a href="#" aria-expanded="false">Locais</a></li>
     <li class="active"><a href="#" aria-expanded="true">Eventos</a></li>
   </ul>
-  <textarea name="timelinePost" rows="6" class="no-border" 
-            placeholder="Write in timeline" style="overflow: hidden; min-height: 6em; height: 52px;">
-            
-  </textarea>
+  <textarea name="timelinePost" rows="6" class="no-border" placeholder="Publique em sua linha do tempo" style="overflow: hidden; min-height: 6em; height: 52px;"></textarea>
   <div class="panel-footer">
     <ul class="navbar-list six columns">
       <li class="navbar-item">
