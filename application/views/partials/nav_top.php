53ceda52eeda217c2fa9433014a8e0055d581a14
00coll <s.eliseu.santos@gmail.com>
2016-09-24 22:17:51 -0300

    Finaliza frontend com informações fixa do perfil do usuário

--- application/views/partials/nav_top.php
+++ application/views/partials/nav_top.php
@@ -2,6 +2,34 @@
   <div class="three columns perfil">
     <div class="info-perfil">
       <img src="/socialmedHUB/assets/imagens/user.png">
+      <p class="margin-bottom-0">Eliseu dos Santos</p>
+      <span class="profissao">Desenvolvedor WEB</span>
+      <p class="margin-bottom-0">
+        <button class="button-success">Adicionar como amigo</button>
+      </p>
+    </div>
+    <div class="sobre-perfil">
+      <p class="margin-bottom-0 text-center">Sobre</p>
+      <ul>
+        <li>
+          <strong><i class="fa fa-users" aria-hidden="true"></i> Seguidores:</strong> 90 Seguidores
+        </li>
+        <li>
+          <strong><i class="fa fa-user" aria-hidden="true"></i> Amigos:</strong> 120
+        </li>
+        <li>
+          <strong><i class="fa fa-user-md" aria-hidden="true"></i> Profissão:</strong> Programador de Sistemas
+        </li>
+      </ul>
+    </div>
+    <div class="fotos-perfil">
+      <p class="margin-bottom-0">Fotos</p>
+      <ul>
+        <li><img src="/socialmedHUB/assets/imagens/user.png"></li>
+        <li><img src="/socialmedHUB/assets/imagens/user.png"></li>
+        <li><img src="/socialmedHUB/assets/imagens/user.png"></li>
+        <li><img src="/socialmedHUB/assets/imagens/user.png"></li>
+      </ul>
     </div>
   </div>
   <div class="six columns">
