9e1436c53ecbf45d641183e6c5fe81a05af02f0f
00coll <s.eliseu.santos@gmail.com>
2016-09-24 23:30:38 -0300

    Adiciona icones no painel de post

--- application/modules/Home/views/home.php
+++ application/modules/Home/views/home.php
@@ -1,1 +1,27 @@
-<span class="text-black">Lista Atividades</span>
+<div class="novo-post">
+  <ul class="nav-post nav-tabs-post">
+    <li class=""><a href="#" aria-expanded="false">Status</a></li>
+    <li class=""><a href="#" aria-expanded="false">Fotos</a></li>
+    <li class=""><a href="#" aria-expanded="false">Locais</a></li>
+    <li class="active"><a href="#" aria-expanded="true">Eventos</a></li>
+  </ul>
+  <textarea name="timelinePost" rows="6" class="no-border" 
+            placeholder="Write in timeline" style="overflow: hidden; min-height: 6em; height: 52px;">
+            
+  </textarea>
+  <div class="panel-footer">
+    <ul class="navbar-list six columns">
+      <li class="navbar-item">
+        <a class="navbar-link no-border-radius no-border" href="#"><i class="fa fa-user-plus" aria-hidden="true"></i></a>
+      </li>
+      <li class="navbar-item">
+        <a class="navbar-link no-border-radius no-border" href="#"><i class="fa fa-map-marker" aria-hidden="true"></i></a>
+      </li>
+      <li class="navbar-item">
+        <a class="navbar-link no-border-radius no-border" href="#">
+          <i class="fa fa-camera-retro" aria-hidden="true"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
